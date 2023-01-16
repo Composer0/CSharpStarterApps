61f51a1937e6c4202c77fabd0bd5ff8eccfcfc61
Orion Palmer <opalmer1989@gmail.com>
2023-01-16 14:55:36 -0500

    Update startup file to include new IHostBUilder

--- Program.cs
+++ Program.cs
@@ -21,6 +21,16 @@ namespace CSharpStarterApps
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+
+                    // For running in Railway
+                    var portVar = Environment.GetEnvironmentVariable("PORT");
+                    if (portVar is { Length: > 0 } && int.TryParse(portVar, out int port))
+                    {
+                        webBuilder.ConfigureKestrel(options =>
+                        {
+                            options.ListenAnyIP(port);
+                        });
+                    }
                 });
     }
 }
